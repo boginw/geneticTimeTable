566f4f5bc51204b3974460c484c0ccc00fa40d5d
sipman <m.hartvigsen@gmail.com>
2016-12-13 09:28:52 +0100

    Gennem gang af noget af det + kommentarer

--- main.c
+++ main.c
@@ -25,71 +25,82 @@
 
 /**
  * ASSUMPTIONS:
- * 	every room has enough space for an entire class
- * 	there are 10 different classyears
+ *  every room has enough space for an entire class
+ *  there are 10 different classyears
  */
 
 int debug = 0;
 
+/* Selve rummet */
 typedef struct room{
-	char name[MAX_NAME_LENGTH];
+    char name[MAX_NAME_LENGTH]; /* Rummets navn */
 } room;
 
+/* Selve klassen */
 typedef struct class{
-	int    year;
-	char   name[MAX_NAME_LENGTH];
-	room*  classRoom;
-	int    maxWorkHoursPerDay;
+    int    year; /* 1b = 1 */
+    char   name[MAX_NAME_LENGTH]; /* klassenavn ex. 1b */
+    room*  classRoom; /* pointer til room struct */
+    int    maxWorkHoursPerDay; /* Maks antal timer man må gå i skole pr dag ! */
 } class;
 
-
+/* Selve faget */
 typedef struct subject{
-	char  name[MAX_NAME_LENGTH];
-	int   perYear[MAX_YEAR];
-	room* roomRequire[MAX_ROOMS];
-	int   roomRequireLength;
-	int   totalHours;
+    char  name[MAX_NAME_LENGTH]; /* navn på faget */
+    int   perYear[MAX_YEAR]; /* timer krævet pr år */
+    room* roomRequire[MAX_ROOMS]; /* array af pointers til kompatible rum, hvis tomt klasse lokale */
+    int   roomRequireLength; /* Antal rum i arrayet oven over */
+    int   totalHours; /* løbende akkumuleret timer */
 } subject;
 
-
+/* Selve læren */
 typedef struct teacher{
-	char     name[MAX_NAME_LENGTH];
-	int      isClassleader;
-	class*   leaderOfClass;
-	subject* canTeach[MAX_SUBJECTS];
-	int      canTeachLength;
-	int      maxWorkHoursPerDay;
+    char     name[MAX_NAME_LENGTH]; /* Lærens navn */
+    int      isClassleader; /* boolean klasselærer eller ej */
+    class*   leaderOfClass; /* pointer til klassen de er klasselærer for */
+    subject* canTeach[MAX_SUBJECTS]; /* array af pointers til kompatible fag */
+    int      canTeachLength; /* Antal pointers i canTeach array'et */
+    int      maxWorkHoursPerDay; /* Maks antal arbejdstimer pr dag */
 } teacher;
 
+/* Timestamp
+ * Således at hour=0 => første lektions tidpunkt på dagens altså 08.00-08.45
+ * osv....
+ * Se dat.sched ## TIMEINTERVALS ## for at se lektions intervalerne
+ */
 typedef struct datetime{
-	int dayOfWeek;
-	int hour;
+    int dayOfWeek;
+    int hour;
 } datetime;
 
+/* Selve lektoren */
 typedef struct lecture{
-	int     init;
-	int     free;
-	room    *l_room;
-	class   *l_class;
-	subject *l_subject;
-	teacher *l_teacher;
-	datetime l_datetime;
+    int     init; /* boolean værdi afgører om den er i et skema eller ej */
+    int     free; /* hvorvidt om det er en fri time */
+    room    *l_room; /* pointer til det rum selve lektionen foregår i */
+    class   *l_class; /* pointer til den pågældende klasse som skal have lektionen */
+    subject *l_subject; /* pointer til faget lektionen omhandler */
+    teacher *l_teacher; /* pointer til den lærer der skal undervise */
+    datetime l_datetime; /* hvilken dag og lektions nummer det skal foregå i */
 } lecture;
 
+/* En enekelt dag i skolen */
 typedef struct schoolDay{
-	int lectureLength;
-	lecture lectures[MAX_LECTURES];
+    int lectureLength; /* antal lektioner på den pågældende dag */
+    lecture lectures[MAX_LECTURES]; /* array af lektioner til dagen */
 } schoolDay;
 
+/* Selve skemaet for en enkelt klasse */
 typedef struct timetable{
-	schoolDay day[WEEK_LENGTH];
-	class *forClass;
+    schoolDay day[WEEK_LENGTH]; /* et array af schoolDay structs */
+    class *forClass; /* pointer til den pågældene klasse. */
 } timetable;
 
+/* Individet (STAVEFEJL ER GRATIS !) */
 typedef struct induvidual{
-	timetable t[MAX_CLASSES];
-	int fitness;
-	int conflicts;
+    timetable t[MAX_CLASSES]; /* Et array af klasseskemaer for en hel skole */
+    int fitness; /* fitness værdien for individet */
+    int conflicts; /* antal konflikter på tværs af skolen i individet. */
 } induvidual;
 
 
@@ -98,141 +109,120 @@ void swapn(void *a, void *b, size_t n);
 int factorial(int n);
 int generateAllCombinations(void *items, size_t size, int sizeOfVariable, void **finalItems);
 int isEmpty(int *array, size_t size);
+int shouldMutate();
 
 #include "fileParse.c"
 #include "lectureControl.c"
 #include "print.c"
 #include "scheduleControl.c"
 
-/* GENETIC FUNCTIONS*/
-int shouldMutate();
-
 int main(int argc, char const *argv[]){
-	/* VARIABLES BEGIN */
+    /* VARIABLES BEGIN */
+    char intervalLabels[MAX_LECTURES][MAX_LABEL_LENGTH]; /* Array af TIMEINTERVALS [se. dat.sched] */
+    room rooms[MAX_ROOMS]; /* Array af rum, bliver deklaret men IKKE initieret */
+    subject subjects[MAX_SUBJECTS]; /* Array af fag, bliver deklaret men IKKE initieret */
+    class classes[MAX_CLASSES]; /* Array af klasser, bliver deklaret men IKKE initieret */
+    teacher teachers[MAX_TEACHERS]; /* Array af lærer, bliver deklaret men IKKE initieret */
+    induvidual induviduals[MAX_INDUVIDUALS]; /* Array af individer (også kendt som populationen), bliver deklaret men IKKE initieret */
+  lecture r_lecture; /* variable til at midlertidig gemme random genereret lektion, indtil de bliver placerer i et klasseskema */
+  int roomCount = 0, subjectCount = 0, classCount = 0, teacherCount = 0; /* variabler til at tælle antal værdier i de enkelte arrays */
+    int i,j; /* iteration counters */
+  int c,d,l,s; /* specifikke iteration counters, hvor c = klasser, d = dage, l = lektioner, s = fag */
+    int seed = time(NULL) * 100; /* Token til at genskabe samme resultater på andre maskiner */
+  int *tempPerYear, subjectIndex; /* specifikke placeholders for data som skal bruges midlertidigt i genereringen */
+    /* VARIABLES END */
 
-	char intervalLabels[MAX_LECTURES][MAX_LABEL_LENGTH];
+    srand(seed); /* Generationen af selve token til genbrug */
 
-	room rooms[MAX_ROOMS];
-	/*room b_rooms[MAX_ROOMS];*/
-	int roomCount = 0;
+  /*
+   * Tjekker efter debug mode
+  */
+    if(argc > 1){
+        if(strcmp(argv[1],"--debug") == 0){
+            debug = 1;
+        }
+    }
 
-	subject subjects[MAX_SUBJECTS];
-	/*subject b_subjects[MAX_SUBJECTS];*/
-	int subjectCount = 0;
+    if(debug){
+        printf("Seed: %d\n", seed);
+    }
 
-	class classes[MAX_CLASSES];
-	/*class b_classes[MAX_CLASSES];*/
-	int classCount = 0;
+  /*
+   * Initierer variablerner, ved at parse dat.sched igennem filParse.c funktionerne
+  */
+    init(rooms,&roomCount,subjects,&subjectCount,classes,&classCount,teachers,&teacherCount,intervalLabels);
 
-	teacher teachers[MAX_TEACHERS];
-	/*teacher b_teachers[MAX_TEACHERS];*/
-	int teacherCount = 0;
-
-	/* needs a better structure... 510000 bytes atm.. */
-	induvidual induviduals[MAX_INDUVIDUALS];
-
-	int i,j;
-	int c,d,l,s;
-	int seed = time(NULL) * 100;
-
-	lecture r_lecture;
-
-	int *tempPerYear;
-	int subjectIndex;
-
-	/* VARIABLES END */
-
-
-	srand(seed);
-
-	if(argc > 1){
-		if(strcmp(argv[1],"--debug") == 0){
-			debug = 1;
-		}
-	}
-
-	if(debug){
-		printf("Seed: %d\n", seed);
-	}
-
-	init(rooms,&roomCount,subjects,&subjectCount,classes,&classCount,teachers,&teacherCount,intervalLabels);
-
-	tempPerYear = malloc(subjectCount * sizeof(int));
+    tempPerYear = malloc(subjectCount * sizeof(int)); /* intierer arrayet således at der er plads til alle fag */
 
 
     /* Create initial population */
+    /* TODO: OMSKRIV DETTE TIL RECURSIVE FUNKTIONS KALD !!!!!! */
     for (i = 0; i < MAX_INDUVIDUALS; i++){
-		induviduals[i].fitness = 0;
+        /* For hvert individ op til maks antal individer */
+        for (c = 0; c < classCount; c++){
+            induviduals[i].t[c].forClass = &classes[c];
+            /* Get all the required hours for class */
+            for (s = 0; s < subjectCount; s++){
+                tempPerYear[s] = ceil(subjects[s].perYear[classes[c].year] / ((float)SCHOOL_DAYS_YEAR / (float)WEEK_LENGTH));
+            }
+            while(!isEmpty(tempPerYear,subjectCount)){
+                for (d = 0; d < WEEK_LENGTH; d++){
+                    for (l = 0; l < MAX_LECTURES; l++){
+                        subjectIndex = randomNumber(0,subjectCount-1);
+                        if(tempPerYear[subjectIndex] != 0 && induviduals[i].t[c].day[d].lectures[induviduals[i].t[c].day[d].lectureLength].init != 1){
+                            r_lecture = randomLectureForClassAndSubject(rooms,roomCount,teachers,teacherCount, &classes[c], &subjects[subjectIndex]);
+                            r_lecture.l_datetime.dayOfWeek = d;
+                            r_lecture.l_datetime.hour = l;
+                            r_lecture.init = 1;
+                            tempPerYear[subjectIndex]--;
+                            induviduals[i].t[c].day[d].lectures[induviduals[i].t[c].day[d].lectureLength++] = r_lecture;
+                        }
+                    }
+                }
+            }
+        }
 
-    	for (c = 0; c < classCount; c++){
-			induviduals[i].t[c].forClass = &classes[c];
-
-			/* Get all the required hours for class */
-			for (s = 0; s < subjectCount; s++){
-    			tempPerYear[s] = ceil(subjects[s].perYear[classes[c].year] / ((float)SCHOOL_DAYS_YEAR / (float)WEEK_LENGTH));
-    		}
-    		
-    		while(!isEmpty(tempPerYear,subjectCount)){
-	    		for (d = 0; d < WEEK_LENGTH; d++){
-
-	    			for (l = 0; l < MAX_LECTURES; l++){
-
-	    				subjectIndex = randomNumber(0,subjectCount-1);
-
-	    				if(tempPerYear[subjectIndex] != 0 && induviduals[i].t[c].day[d].lectures[induviduals[i].t[c].day[d].lectureLength].init != 1){
-							r_lecture = randomLectureForClassAndSubject(rooms,roomCount,teachers,teacherCount, &classes[c], &subjects[subjectIndex]);
-							r_lecture.l_datetime.dayOfWeek = d;
-							r_lecture.l_datetime.hour = l;
-							r_lecture.init = 1;
-							tempPerYear[subjectIndex]--;
-							induviduals[i].t[c].day[d].lectures[induviduals[i].t[c].day[d].lectureLength++] = r_lecture;
-	    				}
-	    			}
-	    		}
-	    	}
-	    }
-
-    	conflicts(&induviduals[i],classCount);
+        conflicts(&induviduals[i],classCount);
     }
 
+    qsort(induviduals, MAX_INDUVIDUALS, sizeof(induvidual), conflictsQsort);
     printf("First conflicts: %3d\n", induviduals[0].conflicts);
-	qsort(induviduals, MAX_INDUVIDUALS, sizeof(induvidual), conflictsQsort);
 
     /* Conflicts preview */
     for (j = 0; j < 10000; j++){
 
-    	for (i = 0; i < MAX_INDUVIDUALS-1; i+=2){
-    		crossover(&induviduals[i], &induviduals[i+1], classCount);
-    	}
+        for (i = 0; i < MAX_INDUVIDUALS-1; i+=2){
+            crossover(&induviduals[i], &induviduals[i+1], classCount);
+        }
 
-	    qsort(induviduals, MAX_INDUVIDUALS, sizeof(induvidual), conflictsQsort);
+        qsort(induviduals, MAX_INDUVIDUALS, sizeof(induvidual), conflictsQsort);
 
-	    if(j%100==0){
-	    	printf("Best conflicts: %3d", induviduals[0].conflicts);
-	    	for (i = 0; i < 19; i++){
-	    		printf("\b");
-	    	}
-	    }
-	}
+        if(j%100==0){
+            printf("Best conflicts: %3d", induviduals[0].conflicts);
+            for (i = 0; i < 19; i++){
+                printf("\b");
+            }
+        }
+    }
 
-	/* Uncomment for demo of schedules */
-	for (i = 0; i < classCount; i++){
-      	printf("\nClass %s, conflicts: %d\n", induviduals[0].t[i].forClass->name, induviduals[0].conflicts);
-    	printTimeTable(induviduals[0].t[i], intervalLabels);
+    /* Uncomment for demo of schedules */
+    for (i = 0; i < classCount; i++){
+        printf("\nClass %s, conflicts: %d\n", induviduals[0].t[i].forClass->name, induviduals[0].conflicts);
+        printTimeTable(induviduals[0].t[i], intervalLabels);
     }
 
 
     /* Conflicts preview */
     /*qsort(induviduals, MAX_INDUVIDUALS, sizeof(induvidual), conflictsQsort);
     for (i = 0; i < MAX_INDUVIDUALS; i++){
-    	printf("Ind: %2d, conflicts: %d\n", i, induviduals[i].conflicts);
+        printf("Ind: %2d, conflicts: %d\n", i, induviduals[i].conflicts);
     }*/
     /* Dump csv files in folder schedules */
     /*dumpCSV(&induviduals[0],classCount,intervalLabels);*/
 
 
     free(tempPerYear);
-	return 0;
+    return 0;
 }
 
 void killTimetable(timetable *t){
@@ -240,18 +230,18 @@ void killTimetable(timetable *t){
 }
 
 int isEmpty(int *array, size_t size){
-	int i;
-	int empty = 0;
-	for (i = 0; i < size; i++){
-		empty += array[i] <= 0;
-	}
-	return empty == size;
+    int i;
+    int empty = 0;
+    for (i = 0; i < size; i++){
+        empty += array[i] <= 0;
+    }
+    return empty == size;
 }
 
 
 int shouldMutate(){
-	int randomnumber;
-	randomnumber = rand() % 100;
+    int randomnumber;
+    randomnumber = rand() % 100;
 
     return randomnumber <= MUTATION_CHANCE;
 }
@@ -263,50 +253,50 @@ int shouldMutate(){
  * @return     random number between min and max
  */
 int randomNumber(int min, int max){
-	return (rand() % (max + 1 - min)) + min;
+    return (rand() % (max + 1 - min)) + min;
 }
 
 /*int generateAllCombinations(void *items, size_t size, int sizeOfVariable, void **finalItems){
-	int i;
-	int j;
-	int k = 0;
-	int endSize = factorial(size);
+    int i;
+    int j;
+    int k = 0;
+    int endSize = factorial(size);
 
-	char *allCombinations	= calloc(endSize * size, sizeOfVariable);
-	char *tempItems 		= calloc(size, sizeOfVariable);
-	*finalItems				= calloc(endSize * size, sizeOfVariable);
+    char *allCombinations   = calloc(endSize * size, sizeOfVariable);
+    char *tempItems         = calloc(size, sizeOfVariable);
+    *finalItems             = calloc(endSize * size, sizeOfVariable);
 
-	memcpy(tempItems, items, sizeOfVariable * size);
+    memcpy(tempItems, items, sizeOfVariable * size);
 
-	for (j = 0; j < endSize; j++){
-		for (i = 0; i < size-1; i++){
-			swapn(
-				&(tempItems[i*sizeOfVariable]),
-				&(tempItems[(i+1)*sizeOfVariable]),
-				sizeOfVariable
-			);
+    for (j = 0; j < endSize; j++){
+        for (i = 0; i < size-1; i++){
+            swapn(
+                &(tempItems[i*sizeOfVariable]),
+                &(tempItems[(i+1)*sizeOfVariable]),
+                sizeOfVariable
+            );
 
-			memcpy(
-				&(allCombinations[(k++)*size*sizeOfVariable]),
-				tempItems,
-				sizeOfVariable*size
-			);
-		}
-	}
-	for (j = 0; j < size; j++){
-		for (i = 0; i < size-1; i++){
+            memcpy(
+                &(allCombinations[(k++)*size*sizeOfVariable]),
+                tempItems,
+                sizeOfVariable*size
+            );
+        }
+    }
+    for (j = 0; j < size; j++){
+        for (i = 0; i < size-1; i++){
 
-		}
-	}
+        }
+    }
 
-	memcpy(
-		*finalItems,
-		allCombinations,
-		endSize*sizeOfVariable
-	);
+    memcpy(
+        *finalItems,
+        allCombinations,
+        endSize*sizeOfVariable
+    );
 
-	free(tempItems);
-	return endSize*size;
+    free(tempItems);
+    return endSize*size;
 }*/
 
 
